ac0de2f05011571bfbf5a771642af912848981f8
smiths9300 <114025453+smiths9300@users.noreply.github.com>
2022-10-06 15:34:19 -0400

    Add files via upload

---
+++ P3T2.py
@@ -0,0 +1,15 @@
+# CTI 110
+# P3T2 - A, B, C
+# S Smith
+# 10/4/22
+
+grade = int(input("Enter number grade: "))
+
+
+if grade >= 90:
+  print("A")
+elif grade >= 80:
+  print("B")
+elif grade <= 80:
+  print("C or less")
+  
