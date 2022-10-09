c3ba8efb2fb6d9774ccfff45a46c78d1c3568f17
smiths9300 <114025453+smiths9300@users.noreply.github.com>
2022-10-09 12:40:30 -0400

    Add files via upload

---
+++ p1w1.py
@@ -0,0 +1,17 @@
+user_num = int(input("Enter integer:\n"))
+
+print("You entered:", user_num)
+
+sq = user_num * user_num
+cube = sq * user_num
+
+print (user_num, "squared is", sq)
+print ("And", user_num, "cubed is", cube, "!!")
+
+user_num2 = int(input("Enter another integer:\n"))
+
+sum = user_num + user_num2
+product = user_num * user_num2
+
+print (user_num, "+", user_num2, "is", sum)
+print (user_num, "*", user_num2, "is", product)
