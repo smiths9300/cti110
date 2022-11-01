ac1a0d96184876a012bd702101ec4e693bba7adc
smiths9300 <smiths9300@student.faytechcc.edu>
2022-11-01 14:10:03 -0400

    Update p4hw1.py

--- p4hw1.py
+++ p4hw1.py
@@ -13,6 +13,15 @@ for grade in range(6):
     grade = int(input("Enter grade: "))
     grades.append(grade)
 
-print(grades)
+print("The grades are: ", grades)
 # max(grades) and min (grades)
 # to show lowest and highest in the list
+print("Highest grade: ", max(grades))
+print("Lowest grade: ", min(grades))
+
+#now the average - divide the sum by the lenght (count)
+total = sum(grades)
+count = len(grades)
+average = total / count
+print("Total is: ", total)
+print("Average is: ", average)
